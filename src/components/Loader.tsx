468935717edf9bd348c17a8df491b588c7727232
Zeyad Zewail <ZeyadZewail@gmail.com>
2023-05-08 16:50:04 +0200

    Hero Section

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
-
+import { Html, useProgress } from "@react-three/drei/web";
 const Loader = () => {
-  return (
-    <div>Loader</div>
-  )
-}
+	const { progress } = useProgress();
 
-export default Loader
+	return (
+		<Html center>
+			<span className="canvas-load"></span>
+			<p
+				style={{
+					fontSize: 36,
+					color: "#f1f1f1",
+					fontWeight: 800,
+					marginTop: 40,
+				}}>
+				{progress.toFixed(2) + "%"}
+			</p>
+		</Html>
+	);
+};
+
+export default Loader;
