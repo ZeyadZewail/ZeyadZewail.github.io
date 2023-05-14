261e1256da5b5c2bd5c07ae8cb286bf155931851
Zeyad Zewail <ZeyadZewail@gmail.com>
2023-05-15 00:08:49 +0200

    path fix

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { output: "export" };
+
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+let assetPrefix = "";
+let basePath = "";
+
+if (isGithubActions) {
+	// trim off `<owner>/`
+	const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
+
+	assetPrefix = `/${repo}/`;
+	basePath = `/${repo}`;
+}
+const nextConfig = { output: "export", assetPrefix: assetPrefix, basePath: basePath };
 
 module.exports = nextConfig;
