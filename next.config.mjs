fa474980bbbb7e79c1e6bd8e0e516b6790182912
Zeyad Zewail <ZeyadZewail@gmail.com>
2023-05-14 23:39:53 +0200

    image fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,28 @@
 /**
  * @type {import('next').NextConfig}
  */
+
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+let assetPrefix = "";
+let basePath = "";
+
+if (isGithubActions) {
+	// trim off `<owner>/`
+	const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
+
+	assetPrefix = `/${repo}/`;
+	basePath = `/${repo}`;
+}
+
 const nextConfig = {
 	output: "export",
 	images: {
 		loader: "akamai",
 		path: "",
 	},
-	assetPrefix: "./",
+	assetPrefix: assetPrefix,
+	basePath: basePath,
 };
 
 export default nextConfig;
