b2cc18024f2970879d6d73f65efc8a73343687d4
Zeyad Zewail <ZeyadZewail@gmail.com>
2023-05-15 00:12:28 +0200

    config revert

--- next.config.js
+++ next.config.js
@@ -1,17 +1,4 @@
 /** @type {import('next').NextConfig} */
-
-const isGithubActions = process.env.GITHUB_ACTIONS || false;
-
-let assetPrefix = "";
-let basePath = "";
-
-if (isGithubActions) {
-	// trim off `<owner>/`
-	const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
-
-	assetPrefix = `/${repo}/`;
-	basePath = `/${repo}`;
-}
-const nextConfig = { output: "export", assetPrefix: assetPrefix, basePath: basePath };
+const nextConfig = { output: "export" };
 
 module.exports = nextConfig;
