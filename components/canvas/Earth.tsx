d83fe7807913fc2a3aea851cad8471acd7b0030c
Zeyad Zewail <ZeyadZewail@gmail.com>
2024-01-13 03:44:36 +0200

    misc

--- components/canvas/Earth.tsx
+++ components/canvas/Earth.tsx
@@ -10,8 +10,7 @@ const Earth = () => {
 
 const EarthCanvas = () => {
 	return (
-		<Canvas
-			shadows
+		<Canvas shadows
 			frameloop="demand"
 			gl={{ preserveDrawingBuffer: true }}
 			camera={{ fov: 45, near: 0.1, far: 200, position: [-4, 3, 6] }}>
